b3da29d58e2a7c5bb027dc39d43ea40a738b5933
John Ramirez <elcuartoespadaulquiorra@gmail.com>
2023-05-21 23:35:11 -0500

    funciones asincronas

--- index.js
+++ index.js
@@ -15,77 +15,105 @@ function printTasks() {
 }
 
 function addTask(description) {
-  tasks.push({
-    description,
-    done: false
+  return new Promise((resolve) => {
+    tasks.push({
+      description,
+      done: false
+    });
+    console.log(`La tarea "${description}" se ha añadido.`);
+    resolve(); // Resuelve la promesa para continuar el flujo
   });
-  console.log(`La tarea "${description}" se ha añadido.`);
 }
 
 function removeTask(index) {
-  const task = tasks[index];
-  tasks.splice(index, 1);
-  console.log(`La tarea "${task.description}" se ha eliminado.`);
+  return new Promise((resolve) => {
+    const task = tasks[index];
+    tasks.splice(index, 1);
+    console.log(`La tarea "${task.description}" se ha eliminado.`);
+    resolve(); // Resuelve la promesa para continuar el flujo
+  });
 }
 
 function completeTask(index) {
-  const task = tasks[index];
-  task.done = true;
-  console.log(`La tarea "${task.description}" se ha completado.`);
-}
-
-function askForTask() {
-  rl.question('Añadir tarea: ', (description) => {
-    addTask(description);
-    printTasks();
-    askForAction();
+  return new Promise((resolve) => {
+    const task = tasks[index];
+    task.done = true;
+    console.log(`La tarea "${task.description}" se ha completado.`);
+    resolve(); // Resuelve la promesa para continuar el flujo
   });
 }
 
-function askForIndexToRemove() {
-  rl.question('Índice de la tarea a eliminar: ', (index) => {
-    removeTask(index - 1);
-    printTasks();
-    askForAction();
+async function askForTask() {
+  return new Promise((resolve) => {
+    rl.question('Añadir tarea: ', async (description) => {
+      await addTask(description); // Espera a que se resuelva la promesa
+      printTasks();
+      resolve(); // Resuelve la promesa para continuar el flujo
+    });
   });
 }
 
-function askForIndexToComplete() {
-  rl.question('Índice de la tarea a completar: ', (index) => {
-    completeTask(index - 1);
-    printTasks();
-    askForAction();
+async function askForIndexToRemove() {
+  return new Promise((resolve) => {
+    rl.question('Índice de la tarea a eliminar: ', async (index) => {
+      await removeTask(index - 1); // Espera a que se resuelva la promesa
+      printTasks();
+      resolve(); // Resuelve la promesa para continuar el flujo
+    });
   });
 }
 
-function askForAction() {
-  rl.question(`
+async function askForIndexToComplete() {
+  return new Promise((resolve) => {
+    rl.question('Índice de la tarea a completar: ', async (index) => {
+      await completeTask(index - 1); // Espera a que se resuelva la promesa
+      printTasks();
+      resolve(); // Resuelve la promesa para continuar el flujo
+    });
+  });
+}
+
+async function askForAction() {
+  return new Promise((resolve) => {
+    rl.question(`
 ¿Qué acción deseas realizar?
   1. Añadir tarea
   2. Eliminar tarea
   3. Completar tarea
   4. Salir
 
-`, (answer) => {
-    switch (answer) {
-      case '1':
-        askForTask();
-        break;
-      case '2':
-        askForIndexToRemove();
-        break;
-      case '3':
-        askForIndexToComplete();
-        break;
-      case '4':
-        rl.close();
-        break;
-      default:
-        console.log('Opción no válida.');
-        askForAction();
-    }
+`, async (answer) => {
+      switch (answer) {
+        case '1':
+          await askForTask(); // Espera a que se resuelva la promesa
+          resolve(); // Resuelve la promesa para continuar el flujo
+          break;
+        case '2':
+          await askForIndexToRemove(); // Espera a que se resuelva la promesa
+          resolve(); // Resuelve la promesa para continuar el flujo
+          break;
+        case '3':
+          await askForIndexToComplete(); // Espera a que se resuelva la promesa
+          resolve(); // Resuelve la promesa para continuar el flujo
+          break;
+        case '4':
+          rl.close();
+          resolve(); // Resuelve la promesa para continuar el flujo
+          break;
+        default:
+          console.log('Opción no válida.');
+          await askForAction(); // Espera a que se resuelva la promesa
+          resolve(); // Resuelve la promesa para continuar el flujo
+      }
+    });
   });
 }
 
-console.log('Bienvenido a la lista de tareas.');
-askForAction();
+async function main() {
+  console.log('Bienvenido a la lista de tareas.');
+  while (true) {
+    await askForAction(); // Espera a que se resuelva la promesa
+  }
+}
+
+main();
